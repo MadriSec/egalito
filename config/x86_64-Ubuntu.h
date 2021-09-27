8fb92b21cb6656345d71b1e744b929c7a832edb1
Ariel Davis <adavis@grammatech.com>
2021-09-27 10:51:10 -0400

    WIP: Working towards unified formatting and infrastructure
    added clang-format and pre-commit
    
    comparing google and llvm  bases
    
    imported .gitlab-ci.yml from prev branch
    
    makefile patches for CI
    
    config patches for CI
    
    updated submodule paths for CI purposes
    
    added symbol.gdb for ubuntu20
    
    trying to get a ref to master in CI
    
    fixed yaml
    
    trying to get a ref to master in CI
    
    updated and fixed clang-format
    
    fixed bug in dep/rtld/Makefile
    
    fixed bug in dep/rtld/Makefile
    
    undoing prev changes
    
    added  running tests in test/script to CI
    
    added ubuntu  dockerfiles
    
    excluding files not in /src/gtirb from formatting
    
    trying out prev .gitmodules with ssh
    
    added more explanation to Makefile changes
    
    reverting changes to .gitmodules
    
    .gitmodules back to ssh
    
    added comment explaining gitconfig manipulations
    
    fixed yaml formatting
    
    using git config instead of echo >> ~/.gitconfig
    
    fixed typo
    
    removed patch for relative urls
    
    back to relative urls; added terminal .git when missing
    
    trying absolute urls now that everything ends in .git
    
    found the right way to to login to gitlab from ci job
    
    checking if this also works over ssh
    
    it doesn't
    
    removed ubuntu20 jobs, since none of the tests pass rn
    
    reverting patch for Ubuntu20 since it seems to produce bad binaries
    
    since we're only formatting src/gtirb, we don't need the comparison logic in check-format

--- config/x86_64-Ubuntu.h
+++ config/x86_64-Ubuntu.h
@@ -7,16 +7,19 @@
 
 /* src */
 
-#define SANDBOX_BASE_ADDRESS    0x40000000
-#define JIT_TABLE_SIZE          64 * 0x1000 // must fit in 32-bit
+#define SANDBOX_BASE_ADDRESS 0x40000000
+#define JIT_TABLE_SIZE 64 * 0x1000  // must fit in 32-bit
+#ifdef __USE_MISC
+#define HAVE_EXPLICIT_BZERO
+#endif
 
 /* app */
 
-#define PROMPT_COLOR    C_GREEN
+#define PROMPT_COLOR C_GREEN
 
 /* test */
 
-#define ANALYSIS_JUMPTABLE_MAIN_COUNT               1
-#define ANALYSIS_JUMPTABLE_PARSE_EXPRESSION_COUNT   2
+#define ANALYSIS_JUMPTABLE_MAIN_COUNT 1
+#define ANALYSIS_JUMPTABLE_PARSE_EXPRESSION_COUNT 2
 
 #endif
