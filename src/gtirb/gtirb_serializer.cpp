ac546735646af2edc2e826ba270b4109b1a03a36
Isaac Pedisich <ipedisich@grammatech.com>
2021-10-18 11:58:04 -0700

    Comments

--- src/gtirb/gtirb_serializer.cpp
+++ src/gtirb/gtirb_serializer.cpp
@@ -20,75 +20,122 @@
 #include "gtirb/Context.hpp"
 #include "gtirb/ByteInterval.hpp"
 
-struct TypeMapping
-{
-    template <typename... Types>
-    using TypeMap = std::unordered_map<std::string, std::tuple<Types...>>;
+/**
+ * Provide a mapping between gtirb and egalito types
+ * (and optionally some auxiliary data)
+ *
+ * TODO: I think this can be maybe simplified to just the
+ * forward- and backward- map and drop the aux-data
+ * (which is definitely more complicated)
+ */
+template <typename EType, typename GType, typename... Aux>
+struct TypeMap {
+    /** Map from egalito type to gtirb type */
+    std::unordered_map<EType, GType> gtirb;
+    /** Map from gtirb type to egalito type */
+    std::unordered_map<GType, EType> egalito;
+    /** Map from gtirb type to additional auxiliary data */
+    std::unordered_map<GType, std::tuple<Aux...>> aux;
 
+    /** Add a mapping between egalito types and gtirb types */
+    void add(EType eobj, GType gobj, Aux... auxobjs) {
+        gtirb[eobj] = gobj;
+        egalito[gobj] = eobj;
+        aux[gobj] = std::make_tuple(auxobjs...);
+    }
+
+    /**
+     * Get the auxiliary data associated with a gtirb object
+     * If T is provided it follows the rules for getting from a tuple
+     * i.e.
+     *     get_aux<gtirb::CodeBlock*>(gtirb_obj)
+     * or
+     *     get_aux<2>(gtirb_obj)
+     * Otherwise, it gets the first provided aux data element
+     *
+     * TODO: Maybe remove this altogether. I'm not sure it's actually necessary.
+     */
+    template <auto T = 0>
+    auto get_aux(GType gobj) {
+        return std::get<T>(aux[gobj]);
+    }
+};
+
+/**
+ * Holds all of the mapping relationships between egalito objects
+ * and their corresponding gtirb objects
+ */
+struct TypeMapper {
     TypeMap<Module *, gtirb::Module *> modules;
     TypeMap<Library *, gtirb::Module *> libraries;
     TypeMap<DataSection *, gtirb::Section *> sections;
-    TypeMap<Function *, gtirb::Symbol *, gtirb::ByteInterval *, gtirb::CodeBlock *> functions;
-    TypeMap<DataVariable *, gtirb::Symbol *, gtirb::ByteInterval *, gtirb::DataBlock *> dataVariables;
-    TypeMap<GlobalVariable *, gtirb::Symbol *, gtirb::ByteInterval *, gtirb::DataBlock *> globalVariables;
+    TypeMap<Function *, gtirb::Symbol *, gtirb::ByteInterval *,
+        gtirb::CodeBlock *>
+        functions;
+    TypeMap<DataVariable *, gtirb::Symbol *, gtirb::ByteInterval *,
+        gtirb::DataBlock *>
+        dataVariables;
+    TypeMap<GlobalVariable *, gtirb::Symbol *, gtirb::ByteInterval *,
+        gtirb::DataBlock *>
+        globalVariables;
     TypeMap<ExternalSymbol *, gtirb::Symbol *> externalSymbols;
 };
 
-class FlatPass : public ChunkVisitor
-{
+/**
+ * Serialization is done in two steps:
+ * First, this class runs through the chunk hierarchy and constructs the
+ * necessary objects and their mapping Then, linking is done between the created
+ * objects to establish the new hierarchy
+ */
+class FlatPass : public ChunkVisitor {
 protected:
     gtirb::Context &C;
-    TypeMapping &TM;
+    TypeMapper &TM;
 
-    // FIXME: Could be multiple elfMap's if the
+    // FIXME: This may not function properly if multiple elfMaps are present due
+    // to deep-scanning a binary
     ElfMap *elfMap;
     template <typename Type>
-    void recurse(Type *root)
-    {
-        for (auto child : root->getChildren()->genericIterable())
-        {
+    void recurse(Type *root) {
+        for (auto child : root->getChildren()->genericIterable()) {
             child->accept(this);
         }
     }
 
 public:
-    FlatPass(gtirb::Context &C, TypeMapping &TM) : C(C), TM(TM){};
+    FlatPass(gtirb::Context &C, TypeMapper &TM) : C(C), TM(TM){};
 
-    virtual void visit(Chunk *chunk) { recurse(chunk); }
-
-    virtual void visit(Program *program) { recurse(program); }
-    virtual void visit(Module *module)
-    {
+    virtual void visit(Module *module) {
         auto *gmodule = gtirb::Module::Create(C, module->getName());
         gmodule->setFileFormat(gtirb::FileFormat::ELF);
 #ifdef ARCH_X86_64
         // FIXME: Can egalito be compiled for multiple ISA?
         gmodule->setISA(gtirb::ISA::X64);
 #endif
-        TM.modules[module->getName()] = std::make_tuple(module, gmodule);
+
+        TM.modules.add(module, gmodule);
         elfMap = module->getElfSpace()->getElfMap();
         recurse(module);
     }
 
-    virtual void visit(Function *function)
-    {
+    virtual void visit(Function *function) {
         auto symbol = function->getSymbol();
         assert(symbol->getAddress() == function->getAddress());
         auto addr = symbol->getAddress();
 
-        auto *interval = gtirb::ByteInterval::Create(C, gtirb::Addr(addr), symbol->getSize());
+        auto *interval = gtirb::ByteInterval::Create(
+            C, gtirb::Addr(addr), symbol->getSize());
         auto intervalBegin = std::as_const(*interval).bytes_begin<char>();
 
-        // Maybe codeBlocks should line up with the "block" in the function as well
+        // Maybe codeBlocks should line up with the "block" in the function as
+        // well
         auto *codeBlock = gtirb::CodeBlock::Create(C, symbol->getSize());
         interval->addBlock(0, codeBlock);
         auto *gSymbol = gtirb::Symbol::Create(C, codeBlock, symbol->getName());
 
         auto start = function->getPosition()->get();
-        for (auto block : CIter::children(function))
-        {
-            for (auto instruction : CIter::children(block))
-            {
+        for (auto block : CIter::children(function)) {
+            for (auto instruction : CIter::children(block)) {
                 auto offset = instruction->getPosition()->get() - start;
                 InstrWriterGetData getter;
                 instruction->getSemantic()->accept(&getter);
@@ -96,53 +143,50 @@ public:
                 auto offsetInterval = intervalBegin + offset;
                 interval->insertBytes(offsetInterval, data.begin(), data.end());
             }
-            TM.functions[symbol->getName()] = std::make_tuple(function, gSymbol, interval, codeBlock);
+            TM.functions.add(function, gSymbol, interval, codeBlock);
             recurse(function);
         }
     }
 
-    virtual void visit(DataSection *dataSection)
-    {
+    virtual void visit(DataSection *dataSection) {
         // DataSections seems to include all of the code sections as well
         auto *section = gtirb::Section::Create(C, dataSection->getName());
-        TM.sections[dataSection->getName()] = std::make_tuple(dataSection, section);
+        TM.sections.add(dataSection, section);
         recurse(dataSection);
     }
 
-    virtual void visit(DataVariable *variable)
-    {
-        // FIXME: I don't understand the difference between this and GlobalVariable
+    virtual void visit(DataVariable *variable) {
+        // FIXME: I don't understand the difference between this and
+        // GlobalVariable
         auto addr = variable->getAddress();
         auto dataBlock = gtirb::DataBlock::Create(C, variable->getSize());
         auto symbol = gtirb::Symbol::Create(C, dataBlock, variable->getName());
-        auto *byteInterval = gtirb::ByteInterval::Create(C, gtirb::Addr(addr), variable->getSize());
-        TM.dataVariables[variable->getName()] = std::make_tuple(variable, symbol, byteInterval, dataBlock);
+        auto *byteInterval = gtirb::ByteInterval::Create(
+            C, gtirb::Addr(addr), variable->getSize());
+        TM.dataVariables.add(variable, symbol, byteInterval, dataBlock);
     }
 
-    virtual void visit(GlobalVariable *variable)
-    {
+    virtual void visit(GlobalVariable *variable) {
         auto addr = variable->getAddress();
         auto dataBlock = gtirb::DataBlock::Create(C, variable->getSize());
         auto symbol = gtirb::Symbol::Create(C, dataBlock, variable->getName());
-        auto *byteInterval = gtirb::ByteInterval::Create(C, gtirb::Addr(addr), variable->getSize());
-        TM.globalVariables[variable->getName()] = std::make_tuple(variable, symbol, byteInterval, dataBlock);
+        auto *byteInterval = gtirb::ByteInterval::Create(
+            C, gtirb::Addr(addr), variable->getSize());
+        TM.globalVariables.add(variable, symbol, byteInterval, dataBlock);
     }
 
-    virtual void visit(Library *library)
-    {
+    virtual void visit(Library *library) {
         // The simplicity of the other mappings is lost a little here,
         // because I am less sure that this cannot be cyclical
-        if (!library->getModule())
-        {
+        if (!library->getModule()) {
             return;
         }
         recurse(library->getModule());
         // Also, both libraries and Modules map onto gtirb::Module's
-        auto gModule = std::get<1>(TM.modules[library->getModule()->getName()]);
-        TM.libraries[library->getName()] = std::make_tuple(library, gModule);
+        auto gModule = TM.modules.gtirb[library->getModule()];
+        TM.libraries.add(library, gModule);
     }
-    virtual void visit(ExternalSymbol *externalSymbol)
-    {
+    virtual void visit(ExternalSymbol *externalSymbol) {
         // TODO:
         // auto symbolName = externalSymbol->getName();
         // if (externalSymbols.find(symbolName) != std::end(externalSymbols)) {
@@ -151,12 +195,17 @@ public:
     }
 
     /** Chunks which are simply recursed into */
+    virtual void visit(Program *program) { recurse(program); }
     virtual void visit(FunctionList *functionList) { recurse(functionList); }
     virtual void visit(PLTList *pltList) { recurse(pltList); }
     virtual void visit(JumpTableList *jumpTableList) { recurse(jumpTableList); }
-    virtual void visit(DataRegionList *dataRegionList) { recurse(dataRegionList); };
+    virtual void visit(DataRegionList *dataRegionList) {
+        recurse(dataRegionList);
+    };
     virtual void visit(VTableList *vtableList) { recurse(vtableList); }
-    virtual void visit(ExternalSymbolList *externalSymbolList) { recurse(externalSymbolList); }
+    virtual void visit(ExternalSymbolList *externalSymbolList) {
+        recurse(externalSymbolList);
+    }
     virtual void visit(LibraryList *libraryList) { recurse(libraryList); }
     virtual void visit(JumpTable *jumpTable) { recurse(jumpTable); }
     virtual void visit(DataRegion *dataRegion) { recurse(dataRegion); }
@@ -170,60 +219,49 @@ public:
     virtual void visit(VTable *vtable) {}
     virtual void visit(VTableEntry *vtableEntry) {}
     virtual void visit(InitFunction *initFunction) {}
-    // Note: Instruction has
+    // Note: Instruction is not totally ignored, but is instead recursed through
+    // manually by Function
     virtual void visit(Instruction *instruction) {}
 };
 
-class GtirbConverter
-{
+class GtirbConverter {
 private:
     gtirb::Context C;
-    TypeMapping TM;
+    TypeMapper TM;
 
 public:
-    // void link_modules(gtirb::IR *ir) {
-    //     for (const auto &[_, value] : TM.modules) {
-    //         const auto &[_, gModule] = value;
-    //         ir->addModule(gModule);
-    //     }
-    // }
-
-    void link_sections(gtirb::Module *gModule, Module *eModule)
-    {
-        for (auto dataRegion : CIter::children(eModule->getDataRegionList()))
-        {
-            for (auto dataSection : CIter::children(dataRegion))
-            {
-                const auto &[_, gSection] = TM.sections[dataSection->getName()];
+    void link_sections(gtirb::Module *gModule, Module *eModule) {
+        for (auto dataRegion : CIter::children(eModule->getDataRegionList())) {
+            for (auto dataSection : CIter::children(dataRegion)) {
+                auto gSection = TM.sections.gtirb[dataSection];
                 gModule->addSection(gSection);
             }
         }
     }
 
-    void link_functions(gtirb::Module *gModule, Module *eModule)
-    {
+    void link_functions(gtirb::Module *gModule, Module *eModule) {
         auto *elfMap = eModule->getElfSpace()->getElfMap();
-        // TypeMap<Function *, gtirb::Symbol *, gtirb::ByteInterval *, gtirb::CodeBlock*> functions;
-        for (const auto &[fName, fValue] : TM.functions)
-        {
-            auto [eFunction, gSymbol, gInterval, Block] = fValue;
+        for (const auto &[eFunction, gSymbol] : TM.functions.gtirb) {
+            auto gInterval = TM.functions.get_aux(gSymbol);
             auto eSymbol = eFunction->getSymbol();
-            auto section = elfMap->findSection(eSymbol->getSectionIndex());
-            auto [eSection, gSection] = TM.sections[section->getName()];
+            // TODO: It's strange to me that I have to go through the elfMap to
+            // get the section but it might actually be the case?
+            auto elfSection = elfMap->findSection(eSymbol->getSectionIndex());
+            auto eSection = eModule->getDataRegionList()->findDataSection(
+                elfSection->getName());
+            auto gSection = TM.sections.gtirb[eSection];
+
             gSection->addByteInterval(gInterval);
             LOG(0, "Adding symbol" << gSymbol->getName());
             gModule->addSymbol(gSymbol);
         }
     }
 
-    gtirb::IR *convert(Program *program)
-    {
+    gtirb::IR *convert(Program *program) {
         FlatPass(C, TM).visit(program);
         auto ir = gtirb::IR::Create(C);
 
-        for (const auto &[name, value] : TM.modules)
-        {
-            auto [eModule, gModule] = value;
+        for (const auto &[eModule, gModule] : TM.modules.gtirb) {
             ir->addModule(gModule);
 
             link_sections(gModule, eModule);
@@ -233,8 +271,7 @@ public:
     }
 };
 
-void GtirbSerializer::serialize(Program *program, std::string filename)
-{
+void GtirbSerializer::serialize(Program *program, std::string filename) {
     LOG(1, "GTIRB serialization");
     GtirbConverter converter;
     auto *converted = converter.convert(program);
