0e0d851516f010eb75b797f90069d787bdbd6a1a
Ariel Davis <adavis@grammatech.com>
2022-02-15 12:05:09 -0600

    no longer copying unused headers from config/

--- relocate.mk
+++ relocate.mk
@@ -2,25 +2,32 @@ binaries=app/etshell app/etcoverage app/etharden app/etobjdump\
   app/etorder app/etprofile app/etsandbox app/ettwocode app/etelf
 libs=src/libegalito.so src/libaddon.so \
   app/libcet.so app/libcoverage.so  app/libsandbox.so
-deps=dep/capstone/libcapstone.so.? \
-  dep/distorm3/make/linux/libdistorm3.so
+deps= dep/distorm3/make/linux/libdistorm3.so
 static_libs=src/libegalito.a
 
+headers=analysis archive break chunk conductor debug\
+  disasm dwarf elf generate gtirb instr load log operation\
+  pass runtime snippet transform util
+header_files := $(foreach dir,$(headers),$(wildcard src/$(dir)/*.h))
+header_files += src/config/config.h
+new_headers := $(foreach f,$(header_files),build/include/$(f))
 .PHONY: all relocate clean
 
 all: relocate
 
 relocate: relocate-lib relocate-bin relocate-dep relocate-dev
-relocate-bin:
-	install -m0755 -D -t ./build/bin ${binaries}
+relocate-bin: ${binaries}
+	install -m0755 -D -t ./build/bin $^
 
-relocate-lib:
-	install -m0644 -D -t ./build/lib ${libs}
+relocate-lib: ${libs}
+	install -m0644 -D -t ./build/lib $^
 
-relocate-dev:
-	mkdir -p ./build/include
-	cd src && cp --parents */*.h ../build/include/
-	cp src/config.h build/include/
+relocate-dev: ${new_headers}
+	@echo "Copying header files"
+
+build/include/%.h: %.h
+	@mkdir -p build/include
+	@cp --parents $< build/include
 
 relocate-dep: 
 	install -m0644 -D -t ./build/lib ${deps}
