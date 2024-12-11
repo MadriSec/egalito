a719414efc34bce8b36e2b2ee519897d4d4a0320
Fatih Durmaz <mail.fatih.durmaz@gmail.com>
2024-12-11 18:29:01 +0000

    Added bookworm to ci
    Signed-off-by: Fatih Durmaz <mail.fatih.durmaz@gmail.com>

--- test/docker/build_x86.sh
+++ test/docker/build_x86.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-REGISTRY_BASE="registry.gitlab.com/egalito/egalito"
+#REGISTRY_BASE="registry.gitlab.com/egalito/egalito"
+REGISTRY_BASE="registry.gitlab.com/madrisec/egalito"
 DOCKERFILE="Dockerfile_x86_64"
 BUILT_IMAGES=()
 
-for BASE_IMAGE in "ubuntu:20.04" "debian:buster"; do {
-    # Gives test image based on ubuntu:20.04 the tag test/ubuntu-20.04
+for BASE_IMAGE in "ubuntu:20.04" "debian:buster" "debian:bookworm"; do {
+    # Gives test image based on BASE_IMAGE the appropriate tag
     # (Tags within the registry can be used for versioning)
     TAG="${REGISTRY_BASE}/test/${BASE_IMAGE//:/-}:gtirb"
     set -x
@@ -26,3 +27,4 @@ echo "===== Use the following commands to push images:"
 for IMAGE in ${BUILT_IMAGES[@]}; do
     echo "docker push $IMAGE"
 done
+
