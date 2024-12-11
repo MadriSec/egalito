df9ca840f4bc42b9736513efcea4a3ee7b1c124b
Fatih Durmaz <mail.fatih.durmaz@gmail.com>
2024-12-11 16:42:27 +0000

    fixing build script
    Signed-off-by: Fatih Durmaz <mail.fatih.durmaz@gmail.com>

--- test/script/get-gtirb.sh
+++ test/script/get-gtirb.sh
@@ -29,16 +29,18 @@ install_gtirb_from_apt() {
 build_and_install_gtirb_from_source() {
     local install_dir=$1
     local repo_url=${2:-"https://github.com/PhiliaTheCat/gtirb.git"}
+    local lib_boost=${3:-"libboost1.67-dev"}
+
+    apt-get install -y "$lib_boost"
 
     mkdir -p "$install_dir"
-    cd "$install_dir"
-    git clone --recursive "$repo_url"
-    cd gtirb
-    mkdir build
-    cd build
+    git clone --recursive "$repo_url" "$install_dir/gtirb"
+    mkdir "$install_dir/gtirb/build"
+    cd "$install_dir/gtirb/build"
     cmake .. -DGTIRB_BUILD_SHARED_LIBS=OFF
     cmake --build . -- -j $(nproc)
-    make install
+    cmake --install .
+    cd -
     rm -rf "$install_dir"
 }
 
@@ -46,11 +48,11 @@ build_and_install_capstone() {
     local install_dir=$1
 
     mkdir -p "$install_dir"
-    cd "$install_dir"
-    git clone --recursive https://github.com/GrammaTech/capstone.git
-    cd capstone
+    git clone --recursive https://github.com/GrammaTech/capstone.git "$install_dir/capstone"
+    cd "$install_dir/capstone"
     CAPSTONE_ARCHS='x86 aarch64' ./make.sh
     CAPSTONE_ARCHS='x86 aarch64' ./make.sh install
+    cd -
     rm -rf "$install_dir"
 }
 
@@ -62,9 +64,8 @@ main() {
         exit
     elif [[ $OS_NAME == "Debian" && $OS_CODENAME == "bookworm" ]]; then
         install_dependencies_common
-        build_and_install_gtirb_from_source /tmp/gtirb-installation "https://github.com/PhiliaTheCat/gtirb.git"
+        build_and_install_gtirb_from_source /tmp/gtirb-installation "https://github.com/PhiliaTheCat/gtirb.git" "libboost1.74-dev"
         build_and_install_capstone /tmp/capstone-installation
-        exit
     else
         install_dependencies_common
         build_and_install_gtirb_from_source /tmp/gtirb-installation "https://github.com/GrammaTech/gtirb.git"
@@ -73,4 +74,3 @@ main() {
 }
 
 main "$@"
-
