cb85565d3bbeecc9ed5071c0605bcc4b34c16300
Fatih Durmaz <mail.fatih.durmaz@gmail.com>
2024-12-11 12:12:23 +0000

    Added Workaround for Smooth Installation In Bookworm
    Signed-off-by: Fatih Durmaz <mail.fatih.durmaz@gmail.com>

--- test/script/get-gtirb.sh
+++ test/script/get-gtirb.sh
@@ -1,47 +1,76 @@
-#!/bin/bash -ex
-# Installs gtirb in a barely-os-agnostic way
+#!/bin/bash
+# Installs GTIRB in a barely-OS-agnostic way
 # If able to be downloaded and installed from apt, it will do so
-# Otherwise, it installs from source by cloning the gtirb repo.
-# Meant to be used as a part of the dockerfile building process
+# Otherwise, it installs from source by cloning the GTIRB repo.
+# Meant to be used as a part of the Dockerfile building process
+
 set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 GTIRB_RELEASE=${1:-"stable"}
+OS_NAME=$(lsb_release -si)
+OS_CODENAME=$(lsb_release -sc)
 
-apt-get update -y
-if [[ "$(lsb_release --short --id)" == "Ubuntu" ]]; then
-    # If we're on ubuntu, attempt to install from apt
+install_dependencies_common() {
+    apt-get update -y
+    apt-get install -y software-properties-common wget gnupg python3-launchpadlib libprotobuf-dev protobuf-compiler cmake git
+}
+
+install_gtirb_from_apt() {
     apt-get install -y software-properties-common wget gnupg
     wget -O - https://download.grammatech.com/gtirb/files/apt-repo/conf/apt.gpg.key | apt-key add -
-    echo "deb https://download.grammatech.com/gtirb/files/apt-repo $(lsb_release --codename --short) $GTIRB_RELEASE" | tee -a /etc/apt/sources.list
+    echo "deb https://download.grammatech.com/gtirb/files/apt-repo $OS_CODENAME $GTIRB_RELEASE" | tee -a /etc/apt/sources.list
 
     add-apt-repository -y ppa:mhier/libboost-latest
     apt-get update -y
     apt-get install -y libcapstone-dev libgtirb-pprinter ddisasm gtirb-pprinter libgtirb-dev libgtirb-dbg
-    exit
-fi
-apt-get install -y software-properties-common
-add-apt-repository ppa:mhier/libboost-latest
-apt-get install -y libprotobuf-dev protobuf-compiler libboost1.67-dev cmake git
+}
 
-# Otherwise, install from source
-# Build & install GTIRB
-mkdir -p /tmp/gtirb-installation
-cd /tmp/gtirb-installation
-git clone --recursive https://github.com/GrammaTech/gtirb.git
-cd gtirb
-mkdir build
-cd build
-cmake .. -DGTIRB_BUILD_SHARED_LIBS=OFF
-cmake --build . -- -j $(nproc)
-make install
-rm -r /tmp/gtirb-installation
+build_and_install_gtirb_from_source() {
+    local install_dir=$1
+    local repo_url=${2:-"https://github.com/PhiliaTheCat/gtirb.git"}
 
-# Build & install Capstone
-mkdir -p /tmp/capstone-installation
-cd /tmp/capstone-installation
-git clone --recursive https://github.com/GrammaTech/capstone.git
-cd capstone
-CAPSTONE_ARCHS='x86 aarch64' ./make.sh
-CAPSTONE_ARCHS='x86 aarch64' ./make.sh install
-rm -r /tmp/capstone-installation
+    mkdir -p "$install_dir"
+    cd "$install_dir"
+    git clone --recursive "$repo_url"
+    cd gtirb
+    mkdir build
+    cd build
+    cmake .. -DGTIRB_BUILD_SHARED_LIBS=OFF
+    cmake --build . -- -j $(nproc)
+    make install
+    rm -rf "$install_dir"
+}
+
+build_and_install_capstone() {
+    local install_dir=$1
+
+    mkdir -p "$install_dir"
+    cd "$install_dir"
+    git clone --recursive https://github.com/GrammaTech/capstone.git
+    cd capstone
+    CAPSTONE_ARCHS='x86 aarch64' ./make.sh
+    CAPSTONE_ARCHS='x86 aarch64' ./make.sh install
+    rm -rf "$install_dir"
+}
+
+main() {
+    apt-get update -y
+
+    if [[ $OS_NAME == "Ubuntu" ]]; then
+        install_gtirb_from_apt
+        exit
+    elif [[ $OS_NAME == "Debian" && $OS_CODENAME == "bookworm" ]]; then
+        install_dependencies_common
+        build_and_install_gtirb_from_source /tmp/gtirb-installation "https://github.com/PhiliaTheCat/gtirb.git"
+        build_and_install_capstone /tmp/capstone-installation
+        exit
+    else
+        install_dependencies_common
+        build_and_install_gtirb_from_source /tmp/gtirb-installation "https://github.com/GrammaTech/gtirb.git"
+        build_and_install_capstone /tmp/capstone-installation
+    fi
+}
+
+main "$@"
+
